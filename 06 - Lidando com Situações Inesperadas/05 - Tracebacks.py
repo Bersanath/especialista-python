26133922b2c09cf6aa5f686d6d6f9f3de4e5c87c
Bersanath <eduardofenix102@gamil.com>
2025-12-17 00:16:19 +0100

    Atualizar

--- 06 - Lidando com Situações Inesperadas/05 - Tracebacks.py
+++ 06 - Lidando com Situações Inesperadas/05 - Tracebacks.py
@@ -8,3 +8,30 @@ Componentes de um Traceback
     NameError: name 'variavel_inexistente' is not defined: O tipo da exceção (ex: NameError, ZeroDivisionError) e uma descrição do erro. 
 '''
 
+def funcao_b():
+    print(10 / 0) # Isso vai gerar um erro
+
+def funcao_a():
+    funcao_b()
+
+funcao_a()
+
+# Traceback gerado:
+
+'''Traceback (most recent call last):
+  File "seu_arquivo.py", line 8, in <module>
+    funcao_a()
+  File "seu_arquivo.py", line 5, in funcao_a
+    funcao_b()
+  File "seu_arquivo.py", line 2, in funcao_b
+    print(10 / 0)
+ZeroDivisionError: division by zero
+'''
+
+'''
+
+Como Usar
+    Leia de baixo para cima: O erro real está na última linha (o tipo de exceção) e o caminho para ele está nas linhas anteriores.
+    Localize o arquivo e linha: Encontre o File "<nome_do_arquivo>", line <numero> para saber onde o erro ocorreu.
+    Identifique o erro: Verifique a mensagem final (ZeroDivisionError, TypeError, etc.) para entender o problema e corrigi-lo no código. 
+'''
